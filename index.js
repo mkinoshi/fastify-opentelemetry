8ba9c06c5fc2226785172a4a1e9bb1230c45e2cc
Makoto Kinoshita <mkinoshi12@gmail.com>
2020-06-12 07:20:35 +0900

    fixed lint errors

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const fp = require('fastify-plugin');
-const get = require('lodash.get');
+const fp = require('fastify-plugin')
+const get = require('lodash.get')
 const attributesNames = {
   FASTIFY_TYPE: 'fastify.type'
-};
+}
 
 const spanNames = {
   ROOT: 'Request',
@@ -13,24 +13,24 @@ const spanNames = {
   SERIALIZATOIN: 'Serialization',
   ON_ERROR: 'OnError',
   ON_SEND: 'OnSend'
-};
+}
 
-function buildRootOption(req, tracePluginOptions) {
-  const urlForHttp = get(req.raw, 'client.parser.incoming.originalUrl', null);
-  const urlForHttp2 = get(req.headers, ':path', null);
-  const url = urlForHttp || urlForHttp2;
-  const methodForHttp = get(req.raw, 'client.parser.incoming.method', null);
-  const methodForHttp2 = get(req.headers, ':method', null);
-  const method = methodForHttp || methodForHttp2;
+function buildRootOption (req, tracePluginOptions) {
+  const urlForHttp = get(req.raw, 'client.parser.incoming.originalUrl', null)
+  const urlForHttp2 = get(req.headers, ':path', null)
+  const url = urlForHttp || urlForHttp2
+  const methodForHttp = get(req.raw, 'client.parser.incoming.method', null)
+  const methodForHttp2 = get(req.headers, ':method', null)
+  const method = methodForHttp || methodForHttp2
 
   return {
     name: tracePluginOptions.nameOverride ? tracePluginOptions.nameOverride(req) : url,
     url,
     method
-  };
+  }
 }
 
-function initializeOpentelemetry(tracer) {
+function initializeOpentelemetry (tracer) {
   return {
     tracer,
     rootSpan: null,
@@ -41,130 +41,130 @@ function initializeOpentelemetry(tracer) {
     serializationSpan: null,
     onErrorSpan: null,
     onSend: null
-  };
+  }
 }
 
-function ignoreFromCondition(ignoreConditions, target) {
+function ignoreFromCondition (ignoreConditions, target) {
   return (
     !target ||
     ignoreConditions.some(condition => {
       if (typeof condition === 'string') {
-        return condition === target;
+        return condition === target
       }
 
       if (typeof condition === 'object') {
-        return target.match(condition);
+        return target.match(condition)
       }
 
       if (typeof condition === 'function') {
-        return condition(target);
+        return condition(target)
       }
     })
-  );
+  )
 }
 
-function plugin(fastify, options = {enabled: true, tracer: null}, next) {
-  const {enabled, tracer, ignoreUrls = [], ignoreMethods = []} = options;
+function plugin (fastify, options = { enabled: true, tracer: null }, next) {
+  const { enabled, tracer, ignoreUrls = [], ignoreMethods = [] } = options
   const commonAttribute = {
     [attributesNames.FASTIFY_TYPE]: 'fastify.hook'
-  };
-
-  if (!tracer) {
-    fastify.log.warn('Tracer is not initialized properly');
   }
 
-  fastify.decorateRequest('opentelemetry', initializeOpentelemetry(tracer));
+  if (!tracer) {
+    fastify.log.warn('Tracer is not initialized properly')
+  }
+
+  fastify.decorateRequest('opentelemetry', initializeOpentelemetry(tracer))
 
   fastify.addHook('onRequest', (req, reply, done) => {
     if (enabled && tracer) {
-      const rootSpanOption = buildRootOption(req, options || {});
+      const rootSpanOption = buildRootOption(req, options || {})
       const shouldCreateSpan =
         !ignoreFromCondition(ignoreUrls, rootSpanOption.url) &&
-        !ignoreFromCondition(ignoreMethods, rootSpanOption.method);
+        !ignoreFromCondition(ignoreMethods, rootSpanOption.method)
       if (shouldCreateSpan) {
-        const rootSpan = tracer.startSpan(rootSpanOption.name, {parent: tracer.getCurrentSpan()});
-        rootSpan.setAttribute('method', rootSpanOption.method);
-        rootSpan.setAttribute('url', rootSpanOption.url);
+        const rootSpan = tracer.startSpan(rootSpanOption.name, { parent: tracer.getCurrentSpan() })
+        rootSpan.setAttribute('method', rootSpanOption.method)
+        rootSpan.setAttribute('url', rootSpanOption.url)
         const onRequestSpan = tracer.startSpan(spanNames.ON_REQUEST, {
           parent: rootSpan,
           ...commonAttribute
-        });
-        req.opentelemetry.rootSpan = rootSpan;
-        req.opentelemetry.onRequestSpan = onRequestSpan;
+        })
+        req.opentelemetry.rootSpan = rootSpan
+        req.opentelemetry.onRequestSpan = onRequestSpan
       }
-      done();
+      done()
     } else {
-      done();
+      done()
     }
-  });
+  })
 
   fastify.addHook('preParsing', (req, reply, done) => {
-    const {tracer, rootSpan, onRequestSpan} = req.opentelemetry;
+    const { tracer, rootSpan, onRequestSpan } = req.opentelemetry
     if (onRequestSpan) {
-      onRequestSpan.end();
-      req.opentelemetry.onRequestSpan = null;
+      onRequestSpan.end()
+      req.opentelemetry.onRequestSpan = null
     }
 
     if (rootSpan) {
       const parsingSpan = tracer.startSpan(spanNames.PARSING, {
         parent: rootSpan,
         ...commonAttribute
-      });
-      req.opentelemetry.parsingSpan = parsingSpan;
+      })
+      req.opentelemetry.parsingSpan = parsingSpan
     }
-    done();
-  });
+    done()
+  })
 
   fastify.addHook('preValidation', (req, reply, done) => {
-    const {tracer, rootSpan, parsingSpan} = req.opentelemetry;
+    const { tracer, rootSpan, parsingSpan } = req.opentelemetry
     if (parsingSpan) {
-      parsingSpan.end();
-      req.opentelemetry.parsingSpan = null;
+      parsingSpan.end()
+      req.opentelemetry.parsingSpan = null
     }
 
     if (rootSpan) {
       const validationSpan = tracer.startSpan(spanNames.VALIDATION, {
         parent: rootSpan,
         ...commonAttribute
-      });
-      req.opentelemetry.validationSpan = validationSpan;
+      })
+      req.opentelemetry.validationSpan = validationSpan
     }
-    done();
-  });
+    done()
+  })
 
   fastify.addHook('preHandler', (req, reply, done) => {
-    const {tracer, rootSpan, validationSpan} = req.opentelemetry;
+    const { tracer, rootSpan, validationSpan } = req.opentelemetry
     if (validationSpan) {
-      validationSpan.end();
-      req.opentelemetry.validationSpan = null;
+      validationSpan.end()
+      req.opentelemetry.validationSpan = null
     }
 
     if (rootSpan) {
       const handlerSpan = tracer.startSpan(spanNames.HANDLER, {
         parent: rootSpan,
         ...commonAttribute
-      });
-      req.opentelemetry.handlerSpan = handlerSpan;
+      })
+      req.opentelemetry.handlerSpan = handlerSpan
     }
-    done();
-  });
+    done()
+  })
 
   fastify.addHook('preSerialization', (req, reply, payload, done) => {
-    const {tracer, rootSpan, handlerSpan} = req.opentelemetry;
+    const { tracer, rootSpan, handlerSpan } = req.opentelemetry
     if (handlerSpan) {
-      handlerSpan.end();
-      req.opentelemetry.handlerSpan = null;
+      handlerSpan.end()
+      req.opentelemetry.handlerSpan = null
     }
 
     if (rootSpan) {
       const serializationSpan = tracer.startSpan(spanNames.SERIALIZATOIN, {
         parent: rootSpan,
         ...commonAttribute
-      });
-      req.opentelemetry.serializationSpan = serializationSpan;
+      })
+      req.opentelemetry.serializationSpan = serializationSpan
     }
-    done();
-  });
+    done()
+  })
 
   fastify.addHook('onError', (req, reply, error, done) => {
     const {
@@ -174,78 +174,78 @@ function plugin(fastify, options = {enabled: true, tracer: null}, next) {
       validationSpan,
       handlerSpan,
       serializationSpan
-    } = req.opentelemetry;
+    } = req.opentelemetry
     if (parsingSpan) {
-      parsingSpan.end();
-      req.opentelemetry.parsingSpan = null;
+      parsingSpan.end()
+      req.opentelemetry.parsingSpan = null
     }
 
     if (validationSpan) {
-      validationSpan.end();
-      req.opentelemetry.validationSpan = null;
+      validationSpan.end()
+      req.opentelemetry.validationSpan = null
     }
 
     if (handlerSpan) {
-      handlerSpan.end();
-      req.opentelemetry.handlerSpan = null;
+      handlerSpan.end()
+      req.opentelemetry.handlerSpan = null
     }
 
     if (serializationSpan) {
-      serializationSpan.end();
-      req.opentelemetry.serializationSpan = null;
+      serializationSpan.end()
+      req.opentelemetry.serializationSpan = null
     }
 
     if (rootSpan) {
       const onErrorSpan = tracer.startSpan(spanNames.ON_ERROR, {
         parent: rootSpan,
         ...commonAttribute
-      });
-      req.opentelemetry.onErrorSpan = onErrorSpan;
+      })
+      req.opentelemetry.onErrorSpan = onErrorSpan
     }
-    done();
-  });
+    done()
+  })
 
   fastify.addHook('onSend', (req, reply, payload, done) => {
-    const {tracer, rootSpan, onErrorSpan, serializationSpan} = req.opentelemetry;
+    const { tracer, rootSpan, onErrorSpan, serializationSpan } = req.opentelemetry
     if (onErrorSpan) {
-      onErrorSpan.end();
-      req.opentelemetry.onErrorSpan = null;
+      onErrorSpan.end()
+      req.opentelemetry.onErrorSpan = null
     }
 
     if (serializationSpan) {
-      serializationSpan.end();
-      req.opentelemetry.serializationSpan = null;
+      serializationSpan.end()
+      req.opentelemetry.serializationSpan = null
     }
 
     if (rootSpan) {
       const onSendSpan = tracer.startSpan(spanNames.ON_SEND, {
         parent: rootSpan,
         ...commonAttribute
-      });
-      req.opentelemetry.onSendSpan = onSendSpan;
+      })
+      req.opentelemetry.onSendSpan = onSendSpan
     }
-    done();
-  });
+    done()
+  })
 
   fastify.addHook('onResponse', (req, reply, done) => {
-    const {rootSpan, onSendSpan} = req.opentelemetry;
+    const { rootSpan, onSendSpan } = req.opentelemetry
     if (onSendSpan) {
-      onSendSpan.end();
-      req.opentelemetry.onSendSpan = null;
+      onSendSpan.end()
+      req.opentelemetry.onSendSpan = null
     }
 
     if (rootSpan) {
-      req.log.info(`New trace<${rootSpan.context().traceId}> was created`);
-      rootSpan.end();
-      req.opentelemetry.rootSpan = null;
+      req.log.info(`New trace<${rootSpan.context().traceId}> was created`)
+      rootSpan.end()
+      req.opentelemetry.rootSpan = null
     }
-    done();
-  });
+    done()
+  })
 
-  next();
+  next()
 }
 
 module.exports = fp(plugin, {
   fastify: '>= 2.0.0',
   name: 'fastify-opentelemetry'
-});
+})
--- test/basic.js
+++ test/basic.js
@@ -1,17 +1,17 @@
-const t = require('tap');
-const split = require('split2');
-const request = require('request');
-const Fastify = require('fastify');
-const api = require('@opentelemetry/api');
-const {SimpleSpanProcessor} = require('@opentelemetry/tracing');
-const {ZipkinExporter} = require('@opentelemetry/exporter-zipkin');
-const {NodeTracerProvider} = require('@opentelemetry/node');
-const fastifyOpentelemetry = require('../');
+const t = require('tap')
+const split = require('split2')
+const request = require('request')
+const Fastify = require('fastify')
+const api = require('@opentelemetry/api')
+const { SimpleSpanProcessor } = require('@opentelemetry/tracing')
+const { ZipkinExporter } = require('@opentelemetry/exporter-zipkin')
+const { NodeTracerProvider } = require('@opentelemetry/node')
+const fastifyOpentelemetry = require('../')
 
 const provider = new NodeTracerProvider({
   logger: console,
   logLevel: 'DEBUG'
-});
+})
 provider.addSpanProcessor(
   new SimpleSpanProcessor(
     new ZipkinExporter({
@@ -19,40 +19,40 @@ provider.addSpanProcessor(
       url: 'http://host.docker.internal:9411/api/v2/spans'
     })
   )
-);
-provider.register();
-const tracer = api.trace.getTracer('fastify-test');
+)
+provider.register()
+const tracer = api.trace.getTracer('fastify-test')
 
 t.test('When you use http for GET method', t => {
-  const stream = split(JSON.parse);
-  const provider = new NodeTracerProvider();
+  const stream = split(JSON.parse)
+  const provider = new NodeTracerProvider()
   provider.addSpanProcessor(
     new SimpleSpanProcessor(
       new ZipkinExporter({
         serviceName: 'fastify'
       })
     )
-  );
+  )
 
   const fastify = Fastify({
     logger: {
       level: 'info',
       stream
     }
-  });
+  })
 
   fastify.register(fastifyOpentelemetry, {
     enabled: true,
     tracer
-  });
+  })
   fastify.get('/user', (req, reply) => {
-    reply.send({hello: 'world'});
-  });
+    reply.send({ hello: 'world' })
+  })
 
   fastify.listen(0, err => {
-    t.plan(4);
-    t.error(err);
-    t.tearDown(() => fastify.close());
+    t.plan(4)
+    t.error(err)
+    t.tearDown(() => fastify.close())
 
     request(
       {
@@ -60,49 +60,49 @@ t.test('When you use http for GET method', t => {
         url: `http://0.0.0.0:${fastify.server.address().port}/user`
       },
       (err, res) => {
-        t.error(err);
-        t.strictEqual(res.statusCode, 200);
+        t.error(err)
+        t.strictEqual(res.statusCode, 200)
       }
-    );
+    )
 
     stream.on('data', log => {
       if (log.msg.match(/New trace<[a-z0-9]+> was created/)) {
-        t.match(log.msg, /New trace<[a-z0-9]+> was created/);
+        t.match(log.msg, /New trace<[a-z0-9]+> was created/)
       }
-    });
-  });
-});
+    })
+  })
+})
 
 t.test('When you use http for POST method', t => {
-  const stream = split(JSON.parse);
-  const provider = new NodeTracerProvider();
+  const stream = split(JSON.parse)
+  const provider = new NodeTracerProvider()
   provider.addSpanProcessor(
     new SimpleSpanProcessor(
       new ZipkinExporter({
         serviceName: 'fastify'
       })
     )
-  );
+  )
 
   const fastify = Fastify({
     logger: {
       level: 'info',
       stream
     }
-  });
+  })
 
   fastify.register(fastifyOpentelemetry, {
     enabled: true,
     tracer
-  });
+  })
   fastify.post('/user', (req, reply) => {
-    reply.send({hello: 'world'});
-  });
+    reply.send({ hello: 'world' })
+  })
 
   fastify.listen(0, err => {
-    t.plan(4);
-    t.error(err);
-    t.tearDown(() => fastify.close());
+    t.plan(4)
+    t.error(err)
+    t.tearDown(() => fastify.close())
 
     request(
       {
@@ -110,51 +110,51 @@ t.test('When you use http for POST method', t => {
         url: `http://0.0.0.0:${fastify.server.address().port}/user`
       },
       (err, res) => {
-        t.error(err);
-        t.strictEqual(res.statusCode, 200);
+        t.error(err)
+        t.strictEqual(res.statusCode, 200)
       }
-    );
+    )
 
     stream.on('data', log => {
       if (log.msg.match(/New trace<[a-z0-9]+> was created/)) {
-        t.match(log.msg, /New trace<[a-z0-9]+> was created/);
+        t.match(log.msg, /New trace<[a-z0-9]+> was created/)
       }
-    });
-  });
-});
+    })
+  })
+})
 
 t.test('Test on ignoreUrls', t => {
-  const stream = split(JSON.parse);
-  const provider = new NodeTracerProvider();
+  const stream = split(JSON.parse)
+  const provider = new NodeTracerProvider()
   provider.addSpanProcessor(
     new SimpleSpanProcessor(
       new ZipkinExporter({
         serviceName: 'fastify'
       })
     )
-  );
+  )
 
   const fastify = Fastify({
     logger: {
       level: 'info',
       stream
     }
-  });
+  })
 
   fastify.register(fastifyOpentelemetry, {
     enabled: true,
     tracer,
     ignoreUrls: [/.*user.*/]
-  });
+  })
   fastify.get('/user', (req, reply) => {
-    reply.send({hello: 'world'});
-  });
+    reply.send({ hello: 'world' })
+  })
 
-  let matchCount = 0;
+  let matchCount = 0
   fastify.listen(0, err => {
-    t.plan(4);
-    t.error(err);
-    t.tearDown(() => fastify.close());
+    t.plan(4)
+    t.error(err)
+    t.tearDown(() => fastify.close())
 
     request(
       {
@@ -162,52 +162,52 @@ t.test('Test on ignoreUrls', t => {
         url: `http://0.0.0.0:${fastify.server.address().port}/user`
       },
       (err, res) => {
-        t.error(err);
-        t.strictEqual(res.statusCode, 200);
-        t.strictEqual(matchCount, 0);
+        t.error(err)
+        t.strictEqual(res.statusCode, 200)
+        t.strictEqual(matchCount, 0)
       }
-    );
+    )
 
     stream.on('data', log => {
       if (log.msg.match(/New trace<[a-z0-9]+> was created/)) {
-        matchCount += 1;
+        matchCount += 1
       }
-    });
-  });
-});
+    })
+  })
+})
 
 t.test('Test on ignoreMethods', t => {
-  const stream = split(JSON.parse);
-  const provider = new NodeTracerProvider();
+  const stream = split(JSON.parse)
+  const provider = new NodeTracerProvider()
   provider.addSpanProcessor(
     new SimpleSpanProcessor(
       new ZipkinExporter({
         serviceName: 'fastify'
       })
     )
-  );
+  )
 
   const fastify = Fastify({
     logger: {
       level: 'info',
       stream
     }
-  });
+  })
 
   fastify.register(fastifyOpentelemetry, {
     enabled: true,
     tracer,
     ignoreMethods: ['GET']
-  });
+  })
   fastify.get('/user', (req, reply) => {
-    reply.send({hello: 'world'});
-  });
+    reply.send({ hello: 'world' })
+  })
 
-  let matchCount = 0;
+  let matchCount = 0
   fastify.listen(0, err => {
-    t.plan(4);
-    t.error(err);
-    t.tearDown(() => fastify.close());
+    t.plan(4)
+    t.error(err)
+    t.tearDown(() => fastify.close())
 
     request(
       {
@@ -215,16 +215,16 @@ t.test('Test on ignoreMethods', t => {
         url: `http://0.0.0.0:${fastify.server.address().port}/user`
       },
       (err, res) => {
-        t.error(err);
-        t.strictEqual(res.statusCode, 200);
-        t.strictEqual(matchCount, 0);
+        t.error(err)
+        t.strictEqual(res.statusCode, 200)
+        t.strictEqual(matchCount, 0)
       }
-    );
+    )
 
     stream.on('data', log => {
       if (log.msg.match(/New trace<[a-z0-9]+> was created/)) {
-        matchCount += 1;
+        matchCount += 1
       }
-    });
-  });
-});
+    })
+  })
+})
